35e0037333fdedac054e278672f35b7bc36eeb87
JoshuaHuang0630 <zqh0630@outlook.com>
2026-05-20 11:02:29 -0700

    Added constructor for HTMLManager

--- HTMLManager.java
+++ HTMLManager.java
@@ -3,4 +3,16 @@ import java.util.*;
 public class HTMLManager
 {
 	private Queue <HTMLTag> tags;
+
+	public HTMLManager(Queue <HTMLTag> html)
+	{
+		if (html == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		for (HTMLTag tag : html)
+		{
+			tags.add(tag);
+		}
+	}
 }
